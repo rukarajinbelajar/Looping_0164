9e1b44de930eb6b1667c452d4045419f840e656d
rukarajinbelajar <maar1101dnd@gmail.com>
2026-03-30 17:56:58 +0700

    memasukkan nilai

--- looping.cpp
+++ looping.cpp
@@ -65,3 +65,15 @@ int main(){
 Logika OR
 #include <iostream>
 using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    
+}
