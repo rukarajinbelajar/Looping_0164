6985b92308e8631fd8063be48c79227f5075a1cd
rukarajinbelajar <maar1101dnd@gmail.com>
2026-03-30 18:35:11 +0700

    menampilkan hasil

--- switchCase.cpp
+++ switchCase.cpp
@@ -37,3 +37,12 @@ void pilihanJurusan() {
             statusKuota = "Error: Kode Tidak Valid";
     }
 }
+
+// prosedur output
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
+
