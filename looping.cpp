3ccf942c367944147f9ab8afec94b2964e1c92e3
rukarajinbelajar <maar1101dnd@gmail.com>
2026-03-30 17:47:39 +0700

    input untuk memasukkan nilai

--- looping.cpp
+++ looping.cpp
@@ -46,4 +46,9 @@ using namespace std;
 int main(){
     float nilB,nilM,rerata;
     string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
 }
